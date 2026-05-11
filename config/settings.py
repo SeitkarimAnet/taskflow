e0c78cc380bb1204798699e1dc3b29a4d1e83ee1
Seitkarim <anet.seitkarim@gmail.com>
2026-05-12 00:26:21 +0500

    Switch to sqlite

--- config/settings.py
+++ config/settings.py
@@ -61,8 +61,8 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # база данных проекта
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('DB_NAME'),
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
         'USER': os.getenv('DB_USER'),
         'PASSWORD': os.getenv('DB_PASSWORD'),
         'HOST': os.getenv('DB_HOST'),
