2514d5317d42bac9d0065baecab79c95df864cc0
Seitkarim <anet.seitkarim@gmail.com>
2026-05-12 22:26:25 +0500

    Add avatar display

--- config/urls.py
+++ config/urls.py
@@ -4,6 +4,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
+
     path('admin/', admin.site.urls),
 
     path('', include('tasks.urls')),
@@ -11,5 +12,12 @@ urlpatterns = [
     path('accounts/', include('accounts.urls')),
 ]
 
+
 if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+    urlpatterns += static(
+
+        settings.MEDIA_URL,
+
+        document_root=settings.MEDIA_ROOT
+    )
