c90b1eccae4de976e724e37c2e8ee19071354cda
Seitkarim <anet.seitkarim@gmail.com>
2026-05-12 20:13:45 +0500

    Fix admin css

--- config/settings.py
+++ config/settings.py
@@ -31,6 +31,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -81,6 +82,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 # папка для static файлов на Render
 STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # дополнительные static файлы
 STATICFILES_DIRS = [BASE_DIR / 'static']
 
